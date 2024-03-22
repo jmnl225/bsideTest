011fbe2213269d5c10875399296cc66b783e6c83
joomin <jmnl225@github.com>
2024-03-22 15:46:10 +0900

    Initial Commit

---
+++ bsideTest/bsideTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  bsideTestApp.swift
+//  bsideTest
+//
+//  Created by joomin on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct bsideTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bsideTestUITests/bsideTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bsideTestUITestsLaunchTests.swift
+//  bsideTestUITests
+//
+//  Created by joomin on 3/22/24.
+//
+
+import XCTest
+
+final class bsideTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
